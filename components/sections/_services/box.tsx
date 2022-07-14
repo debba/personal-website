e252aeb021b33def9c5c3cc592356cb1b1da0541
Andrea Debernardi <andrea@debbaweb.it>
2022-07-14 20:40:05 +0200

    key issue in service box

--- components/sections/_services/box.tsx
+++ components/sections/_services/box.tsx
@@ -11,9 +11,9 @@ export const ServiceBox : React.FC<{ service: ServiceItem, key: number }> = ({se
                     <p className="py-2 text-sm font-normal font-roboto">{service.description}</p>
                 </div>
                 <div className="flex flex-wrap justify-center space-x-2 p-2 w-full">
-                    {service.skills.map((skill, key) => (
+                    {service.skills.map((skill, _key) => (
                         <span
-                            key={key}
+                            key={_key}
                             className="px-4 py-2 mt-2 block rounded-full text-gray-500 bg-gray-200 font-roboto font-semibold text-xs align-center w-max cursor-pointer active:bg-gray-300 transition duration-300 ease">
                             {skill}
                           </span>
