8f5d0ba759802fd86ec4c5f8e888ccceb6f1da2f
Andrea Debernardi <andrea@debbaweb.it>
2022-07-14 20:48:45 +0200

    info message on send

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -76,7 +76,6 @@ export const ContactFormSection = () => {
 
             const { error } = await res.json();
             if (error) {
-                console.log(error);
                 setShowSuccessMessage(false);
                 setShowFailureMessage(true);
                 setButtonText("Send");
@@ -92,57 +91,75 @@ export const ContactFormSection = () => {
     <section id="contact" className="my-1 md:my-[100px]">
         <h2 className="font-roboto text-secondary-color text-center font-extralight uppercase text-3xl md:text-5xl text-white mb-4 leading-normal break-words tracking-tight">CONTACT
             ME</h2>
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div className="grid grid-cols-2 gap-4">
-                    <div className="flex flex-col">
-                        <label htmlFor="first-name">First name</label>
-                        <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} id="first-name" name="first-name" className="text-gray-800 form-input px-3 py-2 rounded-md"
-                               required/>
-                    </div>
-                    <div className="flex flex-col">
-                        <label htmlFor="last-name">Last name</label>
-                        <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}  id="last-name" name="last-name" className="text-gray-800 form-input px-3 py-2 rounded-md"
-                               required/>
-                    </div>
-                    <div className="flex flex-col">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email" name="email" className="text-gray-800 form-input px-3 py-2 rounded-md"
-                               required/>
-                    </div>
-                    <div className="flex flex-col">
-                        <label htmlFor="phone">
-                            <div className="flex align-items">
-                                Phone
-                                <span className="ml-auto opacity-75">Optional</span>
-                            </div>
-                        </label>
-                        <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} id="phone" name="phone" className="text-gray-800 form-input px-3 py-2 rounded-md"/>
-                    </div>
-                    <div className="flex flex-col col-span-2">
-                        <label htmlFor="subject">Subject</label>
-                        <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)} id="subject" name="subject" className="text-gray-800 form-input px-3 py-2 rounded-md"
-                               required/>
-                    </div>
-                    <div className="flex flex-col col-span-2">
-                        <label htmlFor="subject">
-                            <div className="flex align-items">
-                                Message
-                                <span className="ml-auto opacity-75">Max. 500 characters</span>
-                            </div>
-                        </label>
-                        <textarea value={message} onChange={(e) => setMessage(e.target.value)} maxLength={500} rows={4} id="subject" name="subject"
-                                  className="text-gray-800 form-input px-3 py-2 rounded-md" required/>
-                    </div>
+        {
+            showSuccessMessage ? (
+                <div className="mt-2 bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
+                    <p className="font-bold">Email has been succesfully sent.</p>
+                    <p className="text-sm">Thanks for contacting me, Ill back to you soon.</p>
                 </div>
-                <div className="flex justify-end py-4">
-                    <button type="submit"
-                            className="bg-secondary-color text-indigo-100  hover:bg-indigo-800 font-bold py-2 px-4 rounded inline-flex items-center">
-                        {buttonText}
-                    </button>
+            ) : (
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <div className="grid grid-cols-2 gap-4">
+                            <div className="flex flex-col">
+                                <label htmlFor="first-name">First name</label>
+                                <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)}
+                                       id="first-name" name="first-name"
+                                       className="text-gray-800 form-input px-3 py-2 rounded-md"
+                                       required/>
+                            </div>
+                            <div className="flex flex-col">
+                                <label htmlFor="last-name">Last name</label>
+                                <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)}
+                                       id="last-name" name="last-name"
+                                       className="text-gray-800 form-input px-3 py-2 rounded-md"
+                                       required/>
+                            </div>
+                            <div className="flex flex-col">
+                                <label htmlFor="email">Email</label>
+                                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} id="email"
+                                       name="email" className="text-gray-800 form-input px-3 py-2 rounded-md"
+                                       required/>
+                            </div>
+                            <div className="flex flex-col">
+                                <label htmlFor="phone">
+                                    <div className="flex align-items">
+                                        Phone
+                                        <span className="ml-auto opacity-75">Optional</span>
+                                    </div>
+                                </label>
+                                <input type="tel" value={phone} onChange={(e) => setPhone(e.target.value)} id="phone"
+                                       name="phone" className="text-gray-800 form-input px-3 py-2 rounded-md"/>
+                            </div>
+                            <div className="flex flex-col col-span-2">
+                                <label htmlFor="subject">Subject</label>
+                                <input type="text" value={subject} onChange={(e) => setSubject(e.target.value)}
+                                       id="subject" name="subject"
+                                       className="text-gray-800 form-input px-3 py-2 rounded-md"
+                                       required/>
+                            </div>
+                            <div className="flex flex-col col-span-2">
+                                <label htmlFor="subject">
+                                    <div className="flex align-items">
+                                        Message
+                                        <span className="ml-auto opacity-75">Max. 500 characters</span>
+                                    </div>
+                                </label>
+                                <textarea value={message} onChange={(e) => setMessage(e.target.value)} maxLength={500}
+                                          rows={4} id="subject" name="subject"
+                                          className="text-gray-800 form-input px-3 py-2 rounded-md" required/>
+                            </div>
+                        </div>
+                        <div className="flex justify-end py-4">
+                            <button type="submit"
+                                    className="bg-secondary-color text-indigo-100  hover:bg-indigo-800 font-bold py-2 px-4 rounded inline-flex items-center">
+                                {buttonText}
+                            </button>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </div>
+            )
+        }
     </section>
     )
 };
