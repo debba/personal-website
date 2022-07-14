8fe60f3178cccf94b6d967d5fef881883aaeab68
Andrea Debernardi <andrea@debbaweb.it>
2022-07-14 17:51:41 +0200

    Services Section

--- components/sections/_services/box.tsx
+++ components/sections/_services/box.tsx
@@ -3,17 +3,18 @@ import {ServiceItem} from "../../../interfaces/props";
 
 export const ServiceBox : React.FC<{ service: ServiceItem, key: number }> = ({service, key}) => {
     return (
-        <div key={key} className="col-span-1 flex flex-col p-6">
+        <div key={key} className="w-full lg:max-w-full lg:flex">
             <div
-                className="py-5 relative z-10 -ml-10 col-span-3 bg-secondary-color rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 dark:ring-inset dark:ring-white/10">
+                className="bg-secondary-color rounded-xl shadow-lg xl:ml-0 dark:shadow-none dark:ring-1 w-full dark:ring-inset dark:ring-white/10 flex flex-col justify-between leading-normal">
                 <div className="p-4">
-                    <div className="text-xl font-bold font-roboto text-center">{service.name}</div>
+                    <h3 className="text-xl font-bold font-roboto text-center">{service.name}</h3>
+                    <p className="py-2 text-sm font-normal font-roboto">{service.description}</p>
                 </div>
-                <div className="flex flex-1 justify-center space-x-2 px-2">
+                <div className="flex flex-wrap justify-center space-x-2 p-2 w-full">
                     {service.skills.map((skill, key) => (
                         <span
                             key={key}
-                            className="px-4 py-2 mt-1 rounded-full text-gray-500 bg-gray-200 font-roboto font-semibold text-xs flex align-center w-max cursor-pointer active:bg-gray-300 transition duration-300 ease">
+                            className="px-4 py-2 mt-2 block rounded-full text-gray-500 bg-gray-200 font-roboto font-semibold text-xs align-center w-max cursor-pointer active:bg-gray-300 transition duration-300 ease">
                             {skill}
                           </span>
                     ))}
--- components/sections/services.tsx
+++ components/sections/services.tsx
@@ -7,7 +7,9 @@ export const ServicesSection : React.FC<{ services: ServiceItem[] }> = ({service
         <section id="services" className="my-1 md:my-[100px]">
             <h2 className="font-roboto text-secondary-color text-center font-extralight uppercase text-3xl md:text-5xl text-white mb-4 leading-normal break-words tracking-tight">Services</h2>
 
-            <div className="container mx-auto p-6 grid grid-cols-2 gap-12">
+
+
+            <div className="p-5 md:p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 gap-5 md:gap-12">
 
                 {services.map((service,  key) => (
                     <ServiceBox service={service} key={key}/>
