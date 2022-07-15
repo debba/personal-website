e3902231b5dec9a215e7fd74e4960532b2d137e1
Andrea Debernardi <andrea@debbaweb.it>
2022-07-15 17:14:42 +0200

    include matomo analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,24 @@ class MyDocument extends Document {
                 <body className="bg-semi-dark">
                 <Main />
                 <NextScript />
+                {
+                    process.env.NODE_ENV === 'production' && (
+                        <script dangerouslySetInnerHTML={{
+                            __html: `
+                    var _paq = window._paq = window._paq || [];
+                    _paq.push(['trackPageView']);
+                    _paq.push(['enableLinkTracking']);
+                    (function() {
+                    var u="//${process.env.NEXT_PUBLIC_MATOMO_URL}/";
+                    _paq.push(['setTrackerUrl', u+'matomo.php']);
+                    _paq.push(['setSiteId', '1']);
+                    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+                    })();
+                    `
+                        }} />
+                    )
+                }
                 </body>
             </Html>
         )
