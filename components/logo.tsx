aeb22ea4a1e2412894a2bbe39f228e13d0e452ec
Andrea Debernardi <andrea@debbaweb.it>
2022-07-15 09:23:45 +0200

    logo - effects

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
 import Link from "next/link";
+import {useState} from "react";
 
 export const Logo = () => {
 
+    const [isHovering, setIsHovered] = useState(false);
+    const onMouseEnter = () => setIsHovered(true);
+    const onMouseLeave = () => setIsHovered(false);
+
     return (
+        <div onMouseEnter={onMouseEnter}
+              onMouseLeave={onMouseLeave} className="text-white hover:text-secondary-color">
             <Link href="/">
-                <a  className="flex items-center flex-shrink-0 text-white mb-3">
-                    <Image src={"/images/logo-single.png"} alt={"Andrea Debernardi"} width={54} height={54}/>
-                    <span className="font-roboto text-lg uppercase tracking-tight hover:text-secondary-color">Andrea Debernardi</span>
+                <a  className="flex items-center flex-shrink-0  mb-3">
+                    <Image src={`/images/logo-single${isHovering ? '-blue': ''}.png`} alt={"Andrea Debernardi"} width={54} height={54}/>
+                    <span className="font-roboto text-lg uppercase tracking-tight">Andrea Debernardi</span>
                 </a>
             </Link>
+        </div>
     )
 }
 
