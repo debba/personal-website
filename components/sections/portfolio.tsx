5d3692e50c9bf5f39b0cd25e38775b484379680e
Andrea Debernardi <andrea@debbaweb.it>
2022-07-15 10:48:33 +0200

    general fixes

--- components/sections/portfolio.tsx
+++ components/sections/portfolio.tsx
@@ -8,7 +8,7 @@ export const PortfolioSection : React.FC<{ customersLogo: string[]  }> = ({custo
         <section id="portfolio" className="py-3 md:py-[100px]">
             <h2 className="font-roboto text-secondary-color text-center font-extralight uppercase text-3xl md:text-5xl text-white mb-4 leading-normal break-words tracking-tight">Portfolio</h2>
 
-            <p className="text-center font-roboto text-xl">A few customers who chose me.</p>
+            <p className="text-center font-roboto text-xl mb-2">A few customers who chose me.</p>
 
             <div className="md:p-10 grid grid-cols-3 md:grid-cols-5 gap-5 md:gap-12">
 
