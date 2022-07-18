8683d4edad3fb1efcfa4831eeea01da8011c1a10
Andrea Debernardi <andrea@debbaweb.it>
2022-07-18 16:41:59 +0200

    Logo component fix

--- components/logo.tsx
+++ components/logo.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import React, {useState} from "react";
 
-export const Logo : React.FC<{onClick: Function}> = ({onClick}) => {
+export const Logo : React.FC<{onClick?: Function}> = ({onClick}) => {
 
     const [isHovering, setIsHovered] = useState(false);
     const onMouseEnter = () => setIsHovered(true);
     const onMouseLeave = () => setIsHovered(false);
 
     return (
-        <div onClick={() => onClick()} onMouseEnter={onMouseEnter}
+        <div onClick={() => onClick && onClick()} onMouseEnter={onMouseEnter}
               onMouseLeave={onMouseLeave} className="text-white hover:text-secondary-color">
             <Link href="/">
                 <a  className="flex items-center flex-shrink-0  mb-3">
