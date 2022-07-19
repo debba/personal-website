7f56b8a5afeec200f856da94b2297814a6c242d3
Andrea Debernardi <andrea@debbaweb.it>
2022-07-19 11:46:31 +0200

    general - fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,7 +9,7 @@ class MyDocument extends Document {
         return (
             <Html className="scroll-smooth">
                 <Head>
-                    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Mono:wght@200;400;600;700&display=swap" rel="stylesheet" />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto&family=Roboto+Mono:wght@200;400;600;700&display=optional" rel="stylesheet" />
                     <title>Andrea Debernardi | Full stack web developer in Genoa. PHP, Python, Node JS, Angular ans so much more.</title>
                     <meta property="og:title" content="Full stack web developer in Genoa. PHP, Python, Node JS, Angular ans so much more." />
                     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
