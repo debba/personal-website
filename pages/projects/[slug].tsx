d126ea82baab6f00de22513077990105a77ce694
Andrea Debernardi <andrea@debbaweb.it>
2022-07-20 17:42:23 +0200

    Open source projects - fix

--- pages/projects/[slug].tsx
+++ pages/projects/[slug].tsx
@@ -86,7 +86,7 @@ const ProjectNamePage: NextPage<ProjectProps> = ({githubData, ...data}) => {
                             </svg>
                         </div>
                         <div
-                            className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
+                            className="relative mx-auto max-w-md px-0 md:px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
                             <div className="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
                                 {
                                     data.image && (
@@ -102,7 +102,7 @@ const ProjectNamePage: NextPage<ProjectProps> = ({githubData, ...data}) => {
                         </div>
                         <div className={"relative text-center mt-5"}>
                             <a href={githubData.html_url} target={"_blank"}
-                               className={`bg-white text-gray-800 hover:bg-slate-500 hover:text-white hover:border hover:border-white font-bold py-2 px-4 rounded inline-flex items-center`} rel="noreferrer">
+                               className={`bg-white text-gray-800 hover:bg-slate-500 hover:text-white hover:border hover:border-white font-bold py-2 px-0 md:px-4 rounded inline-flex items-center`} rel="noreferrer">
                                 <svg xmlns="http://www.w3.org/2000/svg" className={`fill-current w-4 h-4 mr-2`} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                     <path strokeLinecap="round" strokeLinejoin="round" d={"M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"} />
                                 </svg>
@@ -112,7 +112,7 @@ const ProjectNamePage: NextPage<ProjectProps> = ({githubData, ...data}) => {
                             </a>
                         </div>
                     </div>
-                    <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
+                    <div className="relative mx-auto max-w-md px-0 md:px-4 sm:max-w-3xl sm:px-6 lg:px-0">
                         <div className="pt-12 sm:pt-16 lg:pt-20">
                             <h3 className="text-xl font-bold font-roboto text-center">{data.short_description}</h3>
                             <div className="mt-6  font-roboto font-semibold text-sm space-y-6"
