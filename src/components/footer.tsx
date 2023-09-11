52368274f93d1ede2ecafbecc8276b094f685759
Andrea Debernardi <andrea@debbaweb.it>
2023-09-11 12:33:04 +0200

    subscription form - small refactoring

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,6 +4,7 @@ import {useG11n} from "next-g11n";
 import {DICTIONARY} from "../i18n/dictionary";
 import Image from "next/image";
 import React, {useRef} from "react";
+import SubscriptionForm from "./sections/_footer/subscriptionform";
 
 export const Footer = () => {
     const {translate: t} = useG11n<typeof DICTIONARY>(DICTIONARY, false);
@@ -119,53 +120,7 @@ export const Footer = () => {
                                 {t('footer_keep_in_touch') as string}
                             </span>
                             </div>
-                            <form
-                                method="POST"
-                                action="https://app.emailchef.com/signupwl/7o22666s726q5s6964223n2234353634227q/en"
-                                encType="multipart/form-data"
-                            >
-                                <label className="block mb-6">
-                                    <span className="text-black dark:text-white text-sm">
-                                        {t('footer_newsletter_email') as string}
-                                    </span>
-                                    <input
-                                        id="field-1" name="field[-1]"
-                                        type="email"
-                                        className="
-            block
-            w-full
-            md:w-1/2
-            mt-1
-            text-secondary-color
-            border-secondary-color
-            dark:text-gray-800
-            dark:border-gray-300
-            rounded-md
-            shadow-sm
-            focus:border-indigo-300
-            focus:ring
-            focus:ring-indigo-200
-            focus:ring-opacity-50
-          "
-                                        placeholder={t('footer_newsletter_email_placeholder') as string}
-                                    />
-                                </label>
-
-                                <input type="hidden" name="form_id" value="4564"/>
-                                <input type="hidden" name="lang" value=""/>
-                                <input type="hidden" name="referrer" id="ec_referrer" value=""/>
-                                <div id="ec_recaptcha"></div>
-                                <div className="mb-6 mt-2">
-                                    <button
-                                        type="submit"
-                                        className="bg-secondary-color text-indigo-100  hover:bg-indigo-800 font-bold py-2 px-4 rounded inline-flex items-center"
-                                    >
-                                        <span>
-                                        {t('footer_newsletter_email_subscribe') as string}
-                                            </span>
-                                    </button>
-                                </div>
-                            </form>
+                            <SubscriptionForm />
                         </div>
                     </div>
                 </div>
